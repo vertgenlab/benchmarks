a55bb3c42fcdb33fd5b4ab74cf5d7fff831abad7
Daniel <daniel.snellings@duke.edu>
2023-07-17 13:33:40 -0400

    readmes

---
+++ gonomics/README.md
@@ -0,0 +1,5 @@
+# gonomics
+
+To run benchmarks the gonomics binaries (specifically simulateSam) and samtools must be installed and in PATH. Pysam must also be installed.
+
+Once dependencies are installed, execute runBenchmarks.sh to replicate benchmarks. 
--- gonomics/runBenchmarks.sh
+++ gonomics/runBenchmarks.sh
@@ -24,4 +24,7 @@ go test -bench=Gtf -benchmem
 # cant use -benchmem for the exec tests so we print the average Maxrss for the process though the formatting can be messy due to how go runs benchmarks
 # elprep automatically uses all procs so limit all benchmarks to 1 proc for comparison
 # 1m bam files not added to repo due to size. Must be rederived with simulateSam before uncommenting next line
-#GOMAXPROCS=1 go test -bench=BamRead1m
+echo "Preparing data for bam reading benchmark"
+simulateSam -n 1000000 testdata/test.fasta stdout | samtools sort > testdata/1m_reads.bam
+samtools index testdata/1m_reads.bam
+GOMAXPROCS=1 go test -bench=BamRead1m
